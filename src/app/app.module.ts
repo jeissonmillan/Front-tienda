ba2fcb11143aed4ace00f82c358bb0dd0e7377dc
jeissonmillan <111451785+jeissonmillan@users.noreply.github.com>
2023-06-01 10:05:39 -0500

    aaaaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { AgregarComponent } from './Persona/agregar/agregar.component';
+import { EditarComponent } from './Persona/editar/editar.component';
+import { ServiceComponent } from './Service/service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    AgregarComponent,
+    EditarComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
