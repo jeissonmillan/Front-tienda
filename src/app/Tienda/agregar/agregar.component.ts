4fb4280d9666cc23f7eba2b00d090893206fb010
jeissonmillan <111451785+jeissonmillan@users.noreply.github.com>
2023-06-01 12:50:25 -0500

    ajuste de base de datos

--- src/app/Tienda/agregar/agregar.component.ts
+++ src/app/Tienda/agregar/agregar.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./agregar.component.css']
 })
 export class AgregarComponent {
+  productos = [
+    { id: 1, producto: 'Producto 1', precio: 10.99 },
+    { id: 2, producto: 'Producto 2', precio: 19.99 },
+    { id: 3, producto: 'Producto 3', precio: 5.99 },
+    { id: 4, producto: 'Producto 4', precio: 7.99 },
+    { id: 5, producto: 'Producto 5', precio: 12.99 }
+  ];
 
+  searchTerm: string = '';
+  carritoCompras: any[] = [];
+
+  get filteredProductos() {
+    return this.productos.filter(producto =>
+      producto.producto.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  agregarAlCarrito(producto: any, cantidad: number) {
+    const productoCarrito = {
+      id: producto.id,
+      producto: producto.producto,
+      precio: producto.precio,
+      cantidad: cantidad
+    };
+    this.carritoCompras.push(productoCarrito);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { ListarComponent } from './Tienda/listar/listar.component';
 import { AgregarComponent } from './Tienda/agregar/agregar.component';
 import { EditarComponent } from './Tienda/editar/editar.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
@@ -17,7 +20,9 @@ import { EditarComponent } from './Tienda/editar/editar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
